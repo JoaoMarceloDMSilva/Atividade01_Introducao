345e07d6a6fd14341dd5e266a253f12fd4c60216
JoaoMarceloDMSilva <95003579+JoaoMarceloDMSilva@users.noreply.github.com>
2022-08-22 16:55:09 -0300

    Questão 44

---
+++ q44_ContadorDegraus.py
@@ -0,0 +1,4 @@
+alturaDegrau = float(input('Altura do degrau: '))
+alturaDesejada = float(input('Altura desejada: '))
+quantDegraus = alturaDesejada/alturaDegrau
+print(f'Suba {quantDegraus:.1f} degrau(s)')
