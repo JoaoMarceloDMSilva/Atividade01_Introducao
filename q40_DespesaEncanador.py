fb63cb3abd9682e4219f0ce1274d3655082d36a5
JoaoMarceloDMSilva <95003579+JoaoMarceloDMSilva@users.noreply.github.com>
2022-08-22 16:53:36 -0300

    Questão 40

---
+++ q40_DespesaEncanador.py
@@ -0,0 +1,4 @@
+diariaEncanador = 30
+diasTrabalho = int(input('Inserir quantidade de dias trabalhados: '))
+valorDescImpst = (diariaEncanador * diasTrabalho) * 0.92
+print(f'Valor a ser pago: {valorDescImpst:.2f}')
