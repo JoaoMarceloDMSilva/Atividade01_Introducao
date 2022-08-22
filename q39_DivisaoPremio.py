db011c74dd95962118ae5be060af2a810017a9e4
JoaoMarceloDMSilva <95003579+JoaoMarceloDMSilva@users.noreply.github.com>
2022-08-22 16:52:03 -0300

    Questão 39

---
+++ q39_DivisaoPremio.py
@@ -0,0 +1,5 @@
+value = 780000
+winner_1 = value * 0.46
+winner_2 = value * 0.32
+winner_3 = value * 0.22
+print(f'Primeiro ganahdor: {winner_1}\nSegundo ganhador: {winner_2}\nTerceiro ganhador: {winner_3}')
